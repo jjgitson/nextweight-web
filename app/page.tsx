01c3a0aac658ed96e86a64c3b3ef4a37da9be73b
jjgitson <113270503+jjgitson@users.noreply.github.com>
2025-12-31 07:54:19 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import OnboardingForm from '@/components/OnboardingForm';
+import OnboardingForm from '../components/OnboardingForm';
 
 export default function Home() {
   const router = useRouter();
